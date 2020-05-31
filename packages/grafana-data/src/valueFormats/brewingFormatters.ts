0af91336d7977363fbebc5a79cbc354aaee14cc4
Luke Cyca <me@lukecyca.com>
2020-05-30 19:44:43 -0700

    Fix brewing units

--- packages/grafana-data/src/valueFormats/brewingFormatters.ts
+++ packages/grafana-data/src/valueFormats/brewingFormatters.ts
@@ -1,28 +1,30 @@
-export const toBrewingP = (value: number): string => {
+import { FormattedValue } from './valueFormats';
+
+export const toBrewingP = (value: number): FormattedValue => {
   if (value === null) {
-    return '';
+    return { text: '' };
   }
 
   value = 135.997 * Math.pow(value, 3) - 630.272 * Math.pow(value, 2) + 1111.14 * value - 616.868;
 
   if (Math.abs(value) < 0.3) {
-    return '0';
+    return { text: '0' };
   } else if (value < -5) {
-    return '0';
+    return { text: '0' };
   }
-  return value.toFixed(1) + '°P';
+  return { text: value.toFixed(1), suffix: '°P' };
 };
 
-export const toBrewingBX = (value: number): string => {
+export const toBrewingBX = (value: number): FormattedValue => {
   if (value === null) {
-    return '';
+    return { text: '' };
   }
   value = ((182.4601 * value - 775.6821) * value + 1262.7794) * value - 669.5622;
 
   if (Math.abs(value) < 0.3) {
-    return '0';
+    return { text: '0' };
   } else if (value < -5) {
-    return '0';
+    return { text: '0' };
   }
-  return value.toFixed(1) + '°Bx';
+  return { text: value.toFixed(1), suffix: '°Bx' };
 };
