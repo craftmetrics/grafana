02a765be36d6fadeb29ca4d7a0b89db1129d475c
Joshua Donaldson <donaldson.josh.96@gmail.com>
2020-09-02 08:17:38 -0700

    remove rounding to 0 when less than -5 or between -0.3 and 0.3 (fixes #34)

--- packages/grafana-data/src/valueFormats/brewingFormatters.ts
+++ packages/grafana-data/src/valueFormats/brewingFormatters.ts
@@ -7,11 +7,6 @@ export const toBrewingP = (value: number): FormattedValue => {
 
   value = 135.997 * Math.pow(value, 3) - 630.272 * Math.pow(value, 2) + 1111.14 * value - 616.868;
 
-  if (Math.abs(value) < 0.3) {
-    return { text: '0' };
-  } else if (value < -5) {
-    return { text: '0' };
-  }
   return { text: value.toFixed(1), suffix: '°P' };
 };
 
@@ -21,10 +16,5 @@ export const toBrewingBX = (value: number): FormattedValue => {
   }
   value = ((182.4601 * value - 775.6821) * value + 1262.7794) * value - 669.5622;
 
-  if (Math.abs(value) < 0.3) {
-    return { text: '0' };
-  } else if (value < -5) {
-    return { text: '0' };
-  }
   return { text: value.toFixed(1), suffix: '°Bx' };
 };
